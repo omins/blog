a8ab30d57c373849904a03aaf1c58aace1a06c24
Kevin Minseok Oh <oms3749@gmail.com>
2023-10-09 23:58:49 +0900

    refactor: img tag to next image component

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+import withPlaiceholder from "@plaiceholder/next";
+import { withContentlayer } from "next-contentlayer";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    deviceSizes: [640, 768, 828],
+    minimumCacheTTL: 604800,
+  },
+};
+
+export default withContentlayer(withPlaiceholder(nextConfig));
