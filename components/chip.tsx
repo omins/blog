4d865e91d3da832d26fb0ae9a1490ffbfd1c3470
Kevin Minseok Oh <oms3749@gmail.com>
2023-10-13 21:20:30 +0900

    chore: increase chip text size

--- components/chip.tsx
+++ components/chip.tsx
@@ -7,7 +7,7 @@ export default function Chip({
 }) {
   return (
     <div
-      className={`whitespace-nowrap rounded-full px-2 py-1 text-xs font-semibold  ${
+      className={`whitespace-nowrap rounded-full px-2 py-1 text-sm font-semibold  ${
         isSelected
           ? "dark:bg-green bg-gray-800 text-white dark:text-white"
           : "dark:hover:bg-green bg-gray-100 text-gray-800 hover:bg-gray-800 hover:text-gray-200 dark:hover:text-white"
