d55c33e5f260159e249947529e7ba9891c2e963a
Kevin Minseok Oh <oms3749@gmail.com>
2023-10-23 18:52:22 +0900

    update: mdx Image component

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -1,8 +1,38 @@
-import NextImage, { ImageProps } from "next/image";
+import Image, { ImageProps } from "next/image";
+import { getBlurDataUrl } from "@/lib/image-utils";
 import { useMDXComponent } from "next-contentlayer/hooks";
 
+async function RoundedImage(props: ImageProps) {
+  const { src } = props;
+
+  if (typeof src !== "string") {
+    return null;
+  }
+
+  const blurDataUrl = await getBlurDataUrl(src);
+
+  return (
+    <Image
+      {...props}
+      blurDataURL={blurDataUrl}
+      placeholder="blur"
+      className="image rounded-lg"
+      alt={props.alt || ""}
+    />
+  );
+}
+
+function Callout(props: { emoji: string; children: React.ReactNode }) {
+  return (
+    <div className="flex w-full items-center rounded bg-gray-200 p-1 px-4 py-4 text-sm text-gray-900  dark:border dark:border-gray-100 dark:bg-gray-700 dark:text-gray-200">
+      <div className="mr-4 flex w-4 items-center">{props.emoji}</div>
+      <div className="callout w-full">{props.children}</div>
+    </div>
+  );
+}
+
 const components = {
-  Image: (props: ImageProps) => <NextImage {...props} />,
+  Image: (props: ImageProps) => <RoundedImage {...props} />,
   Callout,
 };
 
@@ -14,13 +44,4 @@ export function Mdx({ code }: MdxProps) {
   const Component = useMDXComponent(code);
 
   return <Component components={components} />;
-}
-
-function Callout(props: { emoji: string; children: React.ReactNode }) {
-  return (
-    <div className="flex w-full items-center rounded bg-gray-200 p-1 px-4 py-4 text-sm text-gray-900  dark:border dark:border-gray-100 dark:bg-gray-700 dark:text-gray-200">
-      <div className="mr-4 flex w-4 items-center">{props.emoji}</div>
-      <div className="callout w-full">{props.children}</div>
-    </div>
-  );
 }
