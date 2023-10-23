f33300f7a045b3a243f7ffe6a9df77cf8f84e942
Kevin Minseok Oh <oms3749@gmail.com>
2023-10-23 16:02:11 +0900

    create: callout component

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -3,6 +3,7 @@ import { useMDXComponent } from "next-contentlayer/hooks";
 
 const components = {
   Image: (props: ImageProps) => <NextImage {...props} />,
+  Callout,
 };
 
 interface MdxProps {
@@ -13,4 +14,13 @@ export function Mdx({ code }: MdxProps) {
   const Component = useMDXComponent(code);
 
   return <Component components={components} />;
+}
+
+function Callout(props: { emoji: string; children: React.ReactNode }) {
+  return (
+    <div className="flex w-full items-center rounded bg-gray-200 p-1 px-4 py-4 text-sm text-gray-900  dark:border dark:border-gray-100 dark:bg-gray-700 dark:text-gray-200">
+      <div className="mr-4 flex w-4 items-center">{props.emoji}</div>
+      <div className="callout w-full">{props.children}</div>
+    </div>
+  );
 }
