80f4cfdd92c891491cc223c6e491d442b63ba447
Kevin Minseok Oh <oms3749@gmail.com>
2023-10-25 23:02:58 +0900

    create: getPosts util

--- lib/posts-utils.ts
+++ lib/posts-utils.ts
@@ -1,9 +1,52 @@
 import { CATEGORY_ALL } from "@/constants/category";
-import { allPosts } from "contentlayer/generated";
+import { Post, allPosts } from "contentlayer/generated";
 
 type CategoryWithCount = {
   [key: string]: number;
 };
+
+type SortBy = "publishedAt" | "title";
+type Order = "desc" | "asc";
+
+export function getPosts(
+  category: string = CATEGORY_ALL,
+  sortBy: SortBy = "publishedAt",
+  order: Order = "desc",
+): Post[] {
+  let posts: Post[] = [...allPosts];
+
+  if (category !== CATEGORY_ALL) {
+    posts = posts.filter((post) => post.category === category);
+  }
+
+  posts = sortPosts(posts, sortBy, order);
+  return posts;
+}
+
+function sortPosts(posts: Post[], sortBy: SortBy, order: Order): Post[] {
+  return posts.sort((a, b) => {
+    const valueA = getValueForSort(a, sortBy);
+    const valueB = getValueForSort(b, sortBy);
+
+    return compareValues(valueA, valueB, order);
+  });
+}
+
+function getValueForSort(post: Post, sortBy: SortBy) {
+  if (sortBy === "publishedAt") {
+    return new Date(post.publishedAt);
+  } else if (sortBy === "title") {
+    return post.title;
+  }
+}
+
+function compareValues<T>(a: T, b: T, order: Order): number {
+  if (order === "desc") {
+    return a > b ? -1 : a < b ? 1 : 0;
+  } else {
+    return a < b ? -1 : a > b ? 1 : 0;
+  }
+}
 
 export function getAllCategoriesWithCount(): {
   name: string;
