be87f4d6316cf8888c732878a336c6984655eba0
Kevin Minseok Oh <oms3749@gmail.com>
2023-10-29 22:50:55 +0900

    create: related-post utils

---
+++ lib/posts/related-posts.ts
@@ -0,0 +1,30 @@
+import { RELATED_POSTS_LIMIT } from "@/constants/posts";
+import { Post } from "contentlayer/generated";
+import { getPosts } from ".";
+
+export function getRelatedPosts(currentPost: Post): Post[] {
+  const posts = getPostsFilteredGivenPost(currentPost);
+
+  return posts.slice(0, RELATED_POSTS_LIMIT);
+}
+
+export function hasMorePosts(currentPost: Post) {
+  const posts = getPostsFilteredGivenPost(currentPost);
+
+  if (posts.length > 4) {
+    return true;
+  }
+
+  return false;
+}
+
+export function getPostsFilteredGivenPost(currentPost: Post) {
+  const { _id, category } = currentPost;
+
+  const allPosts = getPosts({ category });
+  console.log(allPosts.map((post) => post.title));
+  const filteredPosts = allPosts.filter((post) => post._id !== _id);
+  console.log(filteredPosts.map((post) => post.title));
+
+  return filteredPosts;
+}
