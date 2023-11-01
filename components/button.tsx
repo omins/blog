b5ef0a87d5ba370cfd32a9f9e37ba7221e117cb3
Kevin Minseok Oh <oms3749@gmail.com>
2023-11-01 16:03:47 +0900

    chore: remove useless class name

--- components/button.tsx
+++ components/button.tsx
@@ -8,7 +8,7 @@ type Props = {
 export default function Button({ onButtonClick, label = "다시 시도" }: Props) {
   return (
     <button
-      className="mt-4 rounded-md bg-gray-300 px-6 py-2 text-xl font-bold text-gray-900 transition-colors"
+      className="mt-4 rounded-md bg-gray-300 px-6 py-2 text-xl font-bold text-gray-900"
       onClick={onButtonClick}
     >
       {label}
