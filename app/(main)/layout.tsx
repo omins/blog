1b84d2beb3c6518effad929ae5fb35b32d7d36e8
Kevin Minseok Oh <oms3749@gmail.com>
2023-11-04 00:00:18 +0900

    refactor: move structured data to main layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,3 +1,5 @@
+import { getRootStructuredData } from "@/lib/metadata";
+import JsonLd from "@/components/json-ld";
 import CategoryNav from "@/components/layout/category-nav";
 
 type LayoutProps = {
@@ -7,6 +9,7 @@ type LayoutProps = {
 export default function MainLayout({ children }: LayoutProps) {
   return (
     <>
+      <JsonLd data={getRootStructuredData()} />
       <CategoryNav />
       {children}
     </>
--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,6 +1,4 @@
-import { getRootStructuredData } from "@/lib/metadata";
 import { getPosts } from "@/lib/posts";
-import JsonLd from "@/components/json-ld";
 import PaginatedPostList from "@/components/layout/paginated-list";
 
 export default async function Home() {
@@ -8,7 +6,6 @@ export default async function Home() {
 
   return (
     <>
-      <JsonLd data={getRootStructuredData()} />
       <PaginatedPostList
         allPosts={allPosts}
         pageNo={1}
