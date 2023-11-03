5bd5a264725da98cec4c697e1295b8af77712974
Kevin Minseok Oh <oms3749@gmail.com>
2023-11-03 21:47:31 +0900

    refactor: layouts to use fragment instead of div

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -6,9 +6,9 @@ type LayoutProps = {
 
 export default function MainLayout({ children }: LayoutProps) {
   return (
-    <div>
+    <>
       <CategoryNav />
       {children}
-    </div>
+    </>
   );
 }
