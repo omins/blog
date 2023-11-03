89bc7cea6547d89dd08b533d8e443ba813b60dc2
Kevin Minseok Oh <oms3749@gmail.com>
2023-11-03 23:51:56 +0900

    add: site name to base og metadata

--- lib/metadata/index.ts
+++ lib/metadata/index.ts
@@ -1,16 +1,17 @@
 import { CATEGORY_ALL, CATEGORY_NAME_LABELS } from "@/constants/category";
 import { getAllCategoriesWithCount } from "@/lib/posts";
 
-export const BASE_OG = {
-  locale: "ko_KR",
-  type: "website",
-};
-
 export const METADATA = {
   title: "OMIN's Blog",
   description: "주로 학습한 내용 혹은 회고글을 올립니다.",
   author: "Minseok OH",
   url: process.env.BASE_URL || "https://omin.dev",
+};
+
+export const BASE_OG = {
+  locale: "ko_KR",
+  type: "website",
+  siteName: METADATA.title,
 };
 
 export function getAllKeywords(): string[] {
