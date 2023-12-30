14cd155551e9d203e6db04cffdd77a00713f8c00
Kevin Minseok Oh <oms3749@gmail.com>
2023-12-30 23:53:51 +0900

    chore: remove comment library temporarily

--- components/comments/index.tsx
+++ components/comments/index.tsx
@@ -5,7 +5,12 @@ import Giscus from "@giscus/react";
 export default function Comments() {
   return (
     <div className="p-4">
-      <Giscus
+      <p className="mb-2 w-full text-center">
+        🚨 댓글 위젯을 불러올 때 간헐적인 오류가 있어
+        <br />
+        라이브러리 수정 중이에요.
+      </p>
+      {/* <Giscus
         repo="omins/blog"
         repoId="R_kgDOKbTTZw"
         category="Comments"
@@ -18,7 +23,7 @@ export default function Comments() {
         theme="preferred_color_scheme"
         lang="ko"
         loading="lazy"
-      />
+      /> */}
     </div>
   );
 }
