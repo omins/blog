4de1068542f432f126f8c308578f8a29459950f4
omins <52988822+omins@users.noreply.github.com>
2025-10-10 01:22:15 +0900

    Replace root redirect endpoint with static page

--- src/pages/index.ts
+++
@@ -1,8 +0,0 @@
-import type { APIRoute } from "astro";
-import { DEFAULT_LOCALE } from "@/config/locales";
-import { getRelativeLocaleUrl } from "astro:i18n";
-
-export const GET: APIRoute = ({ request, redirect }) => {
-  const search = new URL(request.url).search;
-  return redirect(`${getRelativeLocaleUrl(DEFAULT_LOCALE)}${search}`);
-};
